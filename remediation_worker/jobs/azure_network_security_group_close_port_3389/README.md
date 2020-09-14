34de296f28007111d9b92b67198791a95b6e7a62
Mohammad Zuber Khan <khanz@vmware.com>
2020-09-14 16:11:04 -0700

    add remediation job for closing RDP access

---
+++ remediation_worker/jobs/azure_network_security_group_close_port_3389/README.md
@@ -0,0 +1,70 @@
+# Close Port 3389 for a Network Security Group
+
+This job blocks public access to port 3389
+
+### Applicable Rule
+
+#### Rule ID:
+5c8c267e7a550e1fb6560c9c
+
+#### Rule Name:
+The security group allows access to Remote Desktop port (3389)
+
+## Getting Started
+
+### Prerequisites
+
+The provided Azure service principal must have permissions to make changes to the network security groups and security
+rules.
+Details for the permissions can be found [here](https://docs.microsoft.com/en-us/azure/virtual-network/manage-network-security-group#permissions):
+
+
+### Running the script
+
+You may run this script using following commands:
+```shell script
+  pip install -r requirements.txt
+  python3 azure_network_security_group_close_port_22.py
+```
+
+## Running the tests
+You may run test using following command under vss-remediation-worker-job-code-python directory:
+```shell script
+    pip install -r requirements-dev.txt
+    python3 -m pytest test
+```
+
+## Deployment
+1. Provision a Virtual Machine
+Create an EC2 instance to use for the worker. The minimum required specifications are 128 MB memory and 1/2 Core CPU.
+2. Setup Docker
+Install Docker on the newly provisioned EC2 instance. You can refer to the [docs here](https://docs.aws.amazon.com/AmazonECS/latest/developerguide/docker-basics.html) for more information.
+3. Deploy the worker image
+SSH into the EC2 instance and run the command below to deploy the worker image:
+```shell script
+  docker run --rm -it --name worker \
+  -e VSS_CLIENT_ID={ENTER CLIENT ID}
+  -e VSS_CLIENT_SECRET={ENTER CLIENT SECRET} \
+  vmware/vss-remediation-worker:latest-python
+```
+
+
+## Contributing
+The Secure State team welcomes contributions from the community. If you wish to contribute code and you have not signed our contributor license agreement (CLA), our bot will update the issue when you open a Pull Request. For any questions about the CLA process, please refer to our [FAQ](https://cla.vmware.com/faq).
+All contributions to this repository must be signed as described on that page. Your signature certifies that you wrote the patch or have the right to pass it on as an open-source patch.
+
+For more detailed information, refer to [CONTRIBUTING.md](../../../CONTRIBUTING.md).
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags).
+
+## Authors
+
+* **VMware Secure State** - *Initial work*
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the Apache License - see the [LICENSE](https://github.com/vmware-samples/secure-state-remediation-jobs/blob/master/LICENSE.txt) file for details
---
+++ test/unit/test_azure_network_security_group_close_port_3389.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2020 VMware Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from azure.mgmt.network.models import NetworkSecurityGroup
+from azure.mgmt.network.models import SecurityRule
+from mock import Mock
+
+from remediation_worker.jobs.azure_network_security_group_close_port_3389.azure_network_security_group_close_port_3389 \
+    import (
+    NetworkSecurityGroupClosePort3389,
+)
+
+
+@pytest.fixture
+def valid_payload1():
+    return """
+{
+    "notificationInfo": {
+        "RuleId": "5c6cc5d903dcc90f363146b6",
+        "Service": "Network",
+        "FindingInfo": {
+            "FindingId": "e1606076-d55c-42c5-9ca7-93e933b1e672",
+            "ObjectId": "security_group_name",
+            "ObjectChain": "{\\"cloudAccountId\\":\\"subscription_id\\", \\"properties\\":[{\\"name\\":\\"ResourceGroup\\",\\"stringV\\":\\"resource_group_name\\",\\"type\\":\\"string\\"}]}",
+            "Region": "region"
+            }
+        }
+}
+"""
+
+
+class TestNetworkSecurityGroupClosePort22(object):
+    def test_parse_payload(self, valid_payload1):
+        params = NetworkSecurityGroupClosePort3389().parse(valid_payload1)
+        assert params["security_group_name"] == "security_group_name"
+        assert params["resource_group_name"] == "resource_group_name"
+        assert params["subscription_id"] == "subscription_id"
+        assert params["region"] == "region"
+
+    def test_remediate_success(self):
+        client = Mock()
+        client.network_security_groups.get.return_value = NetworkSecurityGroup(
+            id="nsg",
+            security_rules=[
+                SecurityRule(
+                    protocol="All",
+                    access="Allow",
+                    direction="Inbound",
+                    source_address_prefix="*",
+                    destination_port_ranges=["22", "3389"],
+                ),
+                SecurityRule(
+                    protocol="All",
+                    access="Allow",
+                    direction="Inbound",
+                    source_address_prefix="*",
+                    destination_port_ranges=["20-30", "3380-3390"],
+                ),
+                SecurityRule(
+                    protocol="All",
+                    access="Allow",
+                    direction="Inbound",
+                    source_address_prefix="*",
+                    destination_port_range="3389",
+                ),
+            ],
+        )
+
+        action = NetworkSecurityGroupClosePort3389()
+        assert action.remediate(client, "security_group_name", "resource_group") == 0
+        assert client.network_security_groups.create_or_update.call_count == 1
+
+        call_args = client.network_security_groups.create_or_update.call_args
+        updated_sg = call_args.args[2]
+        security_rules = updated_sg.security_rules
+        assert len(security_rules) == 2
+        assert security_rules[0].destination_port_ranges == ["22"]
+        assert security_rules[1].destination_port_ranges == ["20-30", "3380-3388", "3390"]
+
+    def test_remediate_with_exception(self):
+        client = Mock()
+        client.network_security_groups.create_or_update.side_effect = Exception
+        action = NetworkSecurityGroupClosePort3389()
+        with pytest.raises(Exception):
+            assert action.remediate(client, "security_group_id", "resource_group")
