53b4407e65894a8e5d3b32b32ffb3c09894eb9bb
Shrutika Kulkarni <shruti.sk59@gmail.com>
2021-03-23 20:12:57 +0530

    PLA-23991 - Remediation job to make Key Vault recoverable

---
+++ remediation_worker/jobs/azure_key_vault_is_recoverable/README.md
@@ -0,0 +1,67 @@
+# Enable Soft Delete and Purge Protection for Key Vault
+
+This job enables Soft Delete and Purge Protection for the Azure Key Vault.
+The Retention period will be for 90 days.
+
+**NOTE -** Enabling Soft Delete and Purge Protection is irreversible.
+
+### Applicable Rule
+
+##### Rule ID:
+e2090e34-3580-4088-a815-2ead6a72700f
+
+##### Rule Name:
+Key Vault should be recoverable
+
+## Getting Started
+### Prerequisites
+The provided Azure service principal must have the following permissions:
+`Microsoft.KeyVault/vaults/read`,
+`Microsoft.KeyVault/vaults/write`
+
+A sample role with requisite permissions can be found [here](minimum_permissions.json)
+
+More information about already builtin roles and permissions can be found [here](https://docs.microsoft.com/en-us/azure/role-based-access-control/built-in-roles)
+
+### Running the script
+You may run this script using following commands:
+
+```shell script
+  pip install -r requirements.txt
+  python3 azure_key_vault_is_recoverable.py
+```
+## Running the tests
+You may run test using following command under vss-remediation-worker-job-code-python directory:
+
+```shell script
+    pip install -r requirements-dev.txt
+    python3 -m pytest test
+```
+## Deployment
+Provision an instance by creating an Azure Virtual Machine to use for the worker. The minimum required specifications are 128 MB memory and 1/2 Core CPU.
+Setup Docker on newly provisioned Azure Virtual Machine instance.You can refer to the [docs here](https://docs.microsoft.com/en-us/previous-versions/azure/virtual-machines/linux/docker-machine) for more information.
+Deploy the worker docker image by SSH into the Azure Virtual Machine instance and run the following commands:
+  ```shell script
+  docker run --rm -it --name {worker_name}\
+  -e VSS_CLIENT_ID={ENTER CLIENT ID}\
+  -e VSS_CLIENT_SECRET={ENTER CLIENT SECRET}\
+  -e AZURE_CLIENT_ID={ENTER AZURE_CLIENT_ID} \
+  -e AZURE_CLIENT_SECRET={ENTER AZURE_CLIENT_SECRET} \
+  -e AZURE_TENANT_ID={ENTER AZURE_TENANT_ID} \
+  vmware/vss-remediation-worker:latest-python
+  ```
+## Contributing
+The Secure State team welcomes contributions from the community. If you wish to contribute code and you have not signed our contributor license agreement (CLA), our bot will update the issue when you open a Pull Request. For any questions about the CLA process, please refer to our [FAQ](https://cla.vmware.com/faq).
+
+All contributions to this repository must be signed as described on that page. Your signature certifies that you wrote the patch or have the right to pass it on as an open-source patch.
+
+For more detailed information, refer to [CONTRIBUTING.md](../../../CONTRIBUTING.md).
+## Versioning
+We use SemVer for versioning. For the versions available, see the tags on this repository.
+
+## Authors
+* **VMware Secure State** - *Initial work*
+See also the list of [contributors](https://github.com/vmware-samples/secure-state-remediation-jobs/graphs/contributors) who participated in this project.
+
+## License
+This project is licensed under the Apache License - see the [LICENSE](https://github.com/vmware-samples/secure-state-remediation-jobs/blob/master/LICENSE.txt) file for details
---
+++ remediation_worker/jobs/azure_key_vault_is_recoverable/azure_key_vault_is_recoverable.py
@@ -0,0 +1,133 @@
+import json
+import os
+import sys
+import logging
+
+from azure.mgmt.keyvault import KeyVaultManagementClient
+from azure.identity import ClientSecretCredential
+from azure.mgmt.keyvault.models import VaultPatchParameters, VaultPatchProperties
+
+logging.basicConfig(level=logging.INFO)
+
+
+class KeyVaultIsRecoverable(object):
+    def parse(self, payload):
+        """Parse payload received from Remediation Service.
+        :param payload: JSON string containing parameters received from the remediation service.
+        :type payload: str.
+        :returns: Dictionary of parsed parameters
+        :rtype: dict
+        :raises: KeyError, JSONDecodeError
+        """
+        remediation_entry = json.loads(payload)
+        notification_info = remediation_entry.get("notificationInfo", None)
+        finding_info = notification_info.get("FindingInfo", None)
+        object_id = finding_info.get("ObjectId", None)
+
+        object_chain = remediation_entry["notificationInfo"]["FindingInfo"][
+            "ObjectChain"
+        ]
+        object_chain_dict = json.loads(object_chain)
+        subscription_id = object_chain_dict["cloudAccountId"]
+        region = finding_info.get("Region")
+        properties = object_chain_dict["properties"]
+
+        logging.info(f"cloud_account_id: {subscription_id}")
+        logging.info(f"region: {region}")
+
+        if object_id is None:
+            raise Exception(
+                "Missing parameters for 'payload.notificationInfo.ObjectId'."
+            )
+        resource_group_name = ""
+        for property in properties:
+            if property["name"] == "ResourceGroup" and property["type"] == "string":
+                resource_group_name = property["stringV"]
+                break
+
+        logging.info("parsed params")
+        logging.info(f"  resource_group_name: {resource_group_name}")
+        logging.info(f"  key_vault_name: {object_id}")
+        logging.info(f"  subscription_id: {subscription_id}")
+        logging.info(f"  region: {region}")
+
+        return {
+            "resource_group_name": resource_group_name,
+            "key_vault_name": object_id,
+            "subscription_id": subscription_id,
+            "region": region,
+        }
+
+    def remediate(self, client, resource_group_name, key_vault_name):
+        """Enable Soft Delete and Purge Protection for Key Vault
+        :param client: Instance of the Azure KeyVaultManagementClient.
+        :param resource_group_name: The name of the resource group.
+        :param key_vault_name: Name of the Key Vault.
+        :type resource_group_name: str.
+        :type key_vault_name: str.
+        :type client: object.
+        :returns: Integer signaling success or failure
+        :rtype: int
+        :raises: msrestazure.azure_exceptions.CloudError
+        """
+
+        logging.info("Enabling Soft Delete and Purge Protection for the Key Vault")
+
+        try:
+            key_vault = client.vaults.get(
+                resource_group_name=resource_group_name, vault_name=key_vault_name,
+            )
+            logging.info("    executing client.vaults.update")
+            logging.info(f"      resource_group_name={resource_group_name}")
+            logging.info(f"      vault_name={key_vault_name}")
+
+            if (
+                key_vault.properties.enable_soft_delete is True
+                and key_vault.properties.enable_purge_protection is None
+            ):
+                client.vaults.update(
+                    resource_group_name=resource_group_name,
+                    vault_name=key_vault_name,
+                    parameters=VaultPatchParameters(
+                        properties=VaultPatchProperties(enable_purge_protection=True,)
+                    ),
+                )
+            elif key_vault.properties.enable_soft_delete is None:
+                client.vaults.update(
+                    resource_group_name=resource_group_name,
+                    vault_name=key_vault_name,
+                    parameters=VaultPatchParameters(
+                        properties=VaultPatchProperties(
+                            enable_soft_delete=True,
+                            soft_delete_retention_in_days=90,
+                            enable_purge_protection=True,
+                        )
+                    ),
+                )
+        except Exception as e:
+            logging.error(f"{str(e)}")
+            raise
+        return 0
+
+    def run(self, args):
+        """Run the remediation job.
+        :param args: List of arguments provided to the job.
+        :type args: list.
+        :returns: int
+        """
+        params = self.parse(args[1])
+
+        credential = ClientSecretCredential(
+            client_id=os.environ.get("AZURE_CLIENT_ID"),
+            client_secret=os.environ.get("AZURE_CLIENT_SECRET"),
+            tenant_id=os.environ.get("AZURE_TENANT_ID"),
+        )
+
+        client = KeyVaultManagementClient(credential, params["subscription_id"])
+        return self.remediate(
+            client, params["resource_group_name"], params["key_vault_name"],
+        )
+
+
+if __name__ == "__main__":
+    sys.exit(KeyVaultIsRecoverable().run(sys.argv))
